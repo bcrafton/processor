575ebc643672d75fb6536e16ff5755a6efbfcbeb
Brian Crafton <crafton.b@husky.neu.edu>
2017-05-10 13:38:03 -0400

    hooking in imem

--- processor/instruction_memory.v
+++ processor/instruction_memory.v
@@ -9,11 +9,13 @@ module instruction_memory(
   input wire clk;
   input wire [`ADDR_WIDTH-1:0] pc; // this should be log2 of imemory size, but should also be width of register since it is pc. but pc dosnt have to be same size as register
 // and instruction does not need to be same size as register.
-  output wire [`INST_WIDTH-1:0] instruction;
-  reg [`INST_WIDTH-1:0] memory [0:`IMEMORY_SIZE-1];
 
-  initial $readmemh("../assembler/prog.hex", memory);
+  output reg [`INST_WIDTH-1:0] instruction;
 
-  assign instruction = memory[pc];
+  always @(*) begin 
+    instruction = $mem_read(pc, `IMEM_ID);
+  end
 
 endmodule
+
+// readmemh
