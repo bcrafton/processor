df3b48a32ea0bdbf25543af4a2bb1a531473a501
Brian Crafton <crafton.b@husky.neu.edu>
2017-05-14 00:36:51 -0400

    file structure good

--- assembler/main.ml
+++ assembler/main.ml
@@ -3,6 +3,7 @@ open Runner
 open Printf
 open Lexing
 open Types
+open Str
 
 let () =
 
@@ -12,13 +13,25 @@ let () =
   let compiled_in = "../test/programs/code/asm/" in 
   let compiled_out = "../test/programs/code/bin/" in
 
+  let rec last (l : string list) = 
+    match l with
+    | [x] -> Some(x)
+    | _::rest -> last rest
+    | [] -> None
+  in
+
   let assemble (in_dir : string) (out_dir : string) =
 
     let help (name : string) = 
-      let input_file = open_in (in_dir ^ name) in
-      let program = assemble_file_to_string name input_file in
-      let outfile = open_out (out_dir ^ name ^ ".hex") in
-      fprintf outfile "%s" program
+      let s = (Str.split (regexp "\\.") name) in
+      let ext = (last s) in 
+      match ext with
+      | Some("s") ->
+        let input_file = open_in (in_dir ^ name) in
+        let program = assemble_file_to_string name input_file in
+        let outfile = open_out (out_dir ^ name ^ ".hex") in
+        fprintf outfile "%s" program
+      | _ -> ()
     in
 
     let filenames = Sys.readdir(in_dir) in
--- compiler/main.ml
+++ compiler/main.ml
@@ -3,23 +3,38 @@ open Runner
 open Printf
 open Lexing
 open Types
-       
+open Str
+
 let () =
 
   let code_in = "../test/programs/code/code/" in 
   let code_out = "../test/programs/code/asm/" in
 
+  let rec last (l : string list) = 
+    match l with
+    | [x] -> Some(x)
+    | _::rest -> last rest
+    | [] -> None
+  in
+
   let compile (in_dir : string) (out_dir : string) = 
 
     let help (name : string) = 
-      let input_file = open_in (in_dir ^ name) in
-      let result = compile_file_to_string name input_file in
-      match result with
-      | Left errs ->
-         printf "Errors:\n%s\n" (ExtString.String.join "\n" (print_errors errs))
-      | Right program -> 
-        let outfile = open_out (out_dir ^ name ^ ".s") in
-        fprintf outfile "%s" program
+      let s = (Str.split (regexp "\\.") name) in
+      let ext = (last s) in 
+      match ext with
+      | Some("bc") ->
+        let input_file = open_in (in_dir ^ name) in
+        let result = compile_file_to_string name input_file in
+        begin
+        match result with
+        | Left errs ->
+           printf "Errors:\n%s\n" (ExtString.String.join "\n" (print_errors errs))
+        | Right program -> 
+          let outfile = open_out (out_dir ^ name ^ ".s") in
+          fprintf outfile "%s" program
+        end
+      | _ -> ()
     in
 
     let filenames = Sys.readdir(in_dir) in
