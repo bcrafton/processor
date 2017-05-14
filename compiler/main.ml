4815718fcf9514d7dd0fa885e6a63ac523002afe
Brian Crafton <crafton.b@husky.neu.edu>
2017-05-13 20:08:21 -0400

    updating compiler

--- compiler/main.ml
+++ compiler/main.ml
@@ -5,10 +5,22 @@ open Lexing
 open Types
        
 let () =
-  let name = Sys.argv.(1) in
-  let input_file = open_in name in
-  match compile_file_to_string name input_file with
-  | Left errs ->
-     printf "Errors:\n%s\n" (ExtString.String.join "\n" (print_errors errs))
-  | Right program -> printf "%s\n" program;;
+
+  let in_dir = "../test/programs/code/src/" in 
+  let out_dir = "../test/programs/assembly/compiled/" in
+
+  let compile (name : string) = 
+    let input_file = open_in (in_dir ^ name) in
+    match compile_file_to_string name input_file with
+    | Left errs ->
+       printf "Errors:\n%s\n" (ExtString.String.join "\n" (print_errors errs))
+    | Right program -> 
+      let outfile = open_out (out_dir ^ name ^ ".hex") in
+      fprintf outfile "%s" program
+
+  in
+  
+  let filenames = Sys.readdir(in_dir) in
+  Array.iter compile filenames
+  
 
