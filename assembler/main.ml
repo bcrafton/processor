a6770ba24471346703e699e7b127e719de8a32a0
Brian Crafton <crafton.b@husky.neu.edu>
2017-05-13 20:32:22 -0400

    assemble main

--- assembler/main.ml
+++ assembler/main.ml
@@ -3,10 +3,30 @@ open Runner
 open Printf
 open Lexing
 open Types
-       
-let () =
-  let name = Sys.argv.(1) in
-  let input_file = open_in name in
-  let program = assemble_file_to_string name input_file in
-  printf "%s\n" program;;
 
+let () =
+
+  let src_dir = "../test/programs/assembly/src/" in 
+  let compiled_dir = "../test/programs/assembly/compiled/" in
+
+  let out_dir = "../test/programs/binary/assembled/" in
+
+  let assemble_src (name : string) = 
+    let input_file = open_in (src_dir ^ name) in
+    let program = assemble_file_to_string name input_file in
+    let outfile = open_out (out_dir ^ name ^ ".hex") in
+    fprintf outfile "%s" program
+  in
+
+  let assemble_compiled (name : string) = 
+    let input_file = open_in (compiled_dir ^ name) in
+    let program = assemble_file_to_string name input_file in
+    let outfile = open_out (out_dir ^ name ^ ".hex") in
+    fprintf outfile "%s" program
+  in
+  
+  let src_filenames = Sys.readdir(src_dir) in
+  Array.iter assemble_src src_filenames;
+
+  let compiled_filenames = Sys.readdir(compiled_dir) in
+  Array.iter assemble_compiled compiled_filenames;
