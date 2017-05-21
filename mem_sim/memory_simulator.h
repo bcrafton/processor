7672dd8963c27133912812f6859694c7270ef8ea
Brian Crafton <crafton.b@husky.neu.edu>
2017-05-20 20:34:42 -0400

    get rid of this

--- mem_sim/memory_simulator.h
+++ mem_sim/memory_simulator.h
@@ -38,10 +38,6 @@ typedef unsigned char BOOL;
 
 ///////////////////////////////////////////
 
-int time_compare(void *o1, void *o2);
-int address_compare(void *o1, void *o2);
-
-///////////////////////////////////////////
 
 
 
