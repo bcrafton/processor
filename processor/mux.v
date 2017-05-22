80e50296bf58f76d0771ed201495ad98b4c48d34
Brian Crafton <crafton.b@husky.neu.edu>
2017-05-21 21:38:50 -0400

    adding 8x3 mux needed for forwarding unit

--- processor/mux.v
+++ processor/mux.v
@@ -45,3 +45,51 @@ module mux4x2
   assign out = (sel == 0) ? in0 : (sel == 1) ? in1 : (sel == 2) ? in2 : in3;
 
 endmodule
+
+module mux8x3
+  #(
+  parameter DATA_WIDTH = 3
+  )
+  (
+  in0,
+  in1, 
+  in2,
+  in3, 
+  sel,
+  out
+  );
+  
+  input wire [DATA_WIDTH-1:0] in0;
+  input wire [DATA_WIDTH-1:0] in1;
+  input wire [DATA_WIDTH-1:0] in2;
+  input wire [DATA_WIDTH-1:0] in3;
+  input wire [DATA_WIDTH-1:0] in4;
+  input wire [DATA_WIDTH-1:0] in5;
+  input wire [DATA_WIDTH-1:0] in6;
+  input wire [DATA_WIDTH-1:0] in7;
+  input wire [2:0] sel;
+
+  output wire [DATA_WIDTH-1:0] out;
+
+  assign out = (sel == 0) ? in0 : 
+               (sel == 1) ? in1 : 
+               (sel == 2) ? in2 : 
+               (sel == 3) ? in3 :
+               (sel == 4) ? in4 :
+               (sel == 5) ? in5 :
+               (sel == 6) ? in6 :
+               in7;
+
+endmodule
+
+
+
+
+
+
+
+
+
+
+
+
