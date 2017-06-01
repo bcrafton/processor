be7a135947b4e128e2fe518d9f5121365ed803d5
Brian Crafton <crafton.b@husky.neu.edu>
2017-06-01 14:16:38 -0700

    clearn branch unit

--- processor/branch_unit.v
+++ processor/branch_unit.v
@@ -25,20 +25,20 @@ module branch_unit(
   
     case(jop)
       `JMP_OP_NOP: flush <= 0;
-      `JMP_OP_J:   flush <= 5'b01000;
-      `JMP_OP_JR:  flush <= 5'b01111;
+      `JMP_OP_J:   flush <= `PIPE_REG_EX_MEM;
+      `JMP_OP_JR:  flush <= `PIPE_REG_EX_MEM | `PIPE_REG_ID_EX | `PIPE_REG_IF_ID | `PIPE_REG_PC;
 
-      `JMP_OP_JEQ: flush <= zero == 1'b1 ? 5'b01111 : 0;
-      `JMP_OP_JNE: flush <= zero == 1'b0 ? 5'b01111 : 0;
+      `JMP_OP_JEQ: flush <= zero == 1'b1 ? `PIPE_REG_EX_MEM | `PIPE_REG_ID_EX | `PIPE_REG_IF_ID | `PIPE_REG_PC : 0;
+      `JMP_OP_JNE: flush <= zero == 1'b0 ? `PIPE_REG_EX_MEM | `PIPE_REG_ID_EX | `PIPE_REG_IF_ID | `PIPE_REG_PC : 0;
 
-      `JMP_OP_JL:  flush <= less == 1'b1 ? 5'b01111 : 0;
-      `JMP_OP_JLE: flush <= less == 1'b1 ? 5'b01111 : 0;
+      `JMP_OP_JL:  flush <= less == 1'b1 ? `PIPE_REG_EX_MEM | `PIPE_REG_ID_EX | `PIPE_REG_IF_ID | `PIPE_REG_PC : 0;
+      `JMP_OP_JLE: flush <= less == 1'b1 ? `PIPE_REG_EX_MEM | `PIPE_REG_ID_EX | `PIPE_REG_IF_ID | `PIPE_REG_PC : 0;
 
-      `JMP_OP_JG:  flush <= greater == 1'b1 ? 5'b01111 : 0;
-      `JMP_OP_JGE: flush <= greater == 1'b1 ? 5'b01111 : 0;
+      `JMP_OP_JG:  flush <= greater == 1'b1 ? `PIPE_REG_EX_MEM | `PIPE_REG_ID_EX | `PIPE_REG_IF_ID | `PIPE_REG_PC : 0;
+      `JMP_OP_JGE: flush <= greater == 1'b1 ? `PIPE_REG_EX_MEM | `PIPE_REG_ID_EX | `PIPE_REG_IF_ID | `PIPE_REG_PC : 0;
 
-      `JMP_OP_JZ:  flush <= zero == 1'b1 ? 5'b01111 : 0;
-      `JMP_OP_JNZ: flush <= zero == 1'b0 ? 5'b01111 : 0;
+      `JMP_OP_JZ:  flush <= zero == 1'b1 ? `PIPE_REG_EX_MEM | `PIPE_REG_ID_EX | `PIPE_REG_IF_ID | `PIPE_REG_PC : 0;
+      `JMP_OP_JNZ: flush <= zero == 1'b0 ? `PIPE_REG_EX_MEM | `PIPE_REG_ID_EX | `PIPE_REG_IF_ID | `PIPE_REG_PC : 0;
 
       //`JMP_OP_JO:  flush <= (zero == 1'b1);
     endcase
