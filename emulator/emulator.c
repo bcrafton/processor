73e8a3b427dd75c22020c9f4dee490d004742d47
Brian Crafton <crafton.b@husky.neu.edu>
2017-06-15 18:57:09 -0400

    emualator

--- emulator/emulator.c
+++ emulator/emulator.c
@@ -14,6 +14,9 @@ typedef unsigned long TIME;
 typedef unsigned char BYTE;
 typedef unsigned char BOOL;
 
+#define LAST(k,n) ((k) & ((1<<(n))-1))
+#define MID(k,m,n) LAST((k)>>(m),((n)-(m)))
+
 #define DMEMORY_SIZE 1024
 #define DATA_WIDTH 32
 
@@ -30,6 +33,22 @@ typedef unsigned char BOOL;
 WORD dmemory[DMEMORY_SIZE];
 REGISTER regfile[REGFILE_SIZE];
 INSTRUCTION imemory[IMEMORY_SIZE];
+
+typedef struct instruction{
+  unsigned int opcode;
+  unsigned int rs;
+  unsigned int rt;
+  unsigned int rd;
+  unsigned int immediate;
+} instruction_t;
+
+// is this really what we want to do here?
+// no this is dumb
+// no need to go through each case
+// not useful at all.
+// 
+instruction_t* to_instruction(INSTRUCTION i, instruction_t* is)
+{}
 
 void load_program()
 {
@@ -55,20 +74,130 @@ void load_program()
   }
 }
 
+// make a function thats like
+// get bits from here to here/
+// 26:21
+// dont work because of 31 case ...
+
+/*
+static inline unsigned int bit_select(INSTRUCTION i, unsigned int msb, unsigned int lsb)
+{
+  unsigned int mask, masked, bits;
+  if (msb == 31) {
+    mask = 0xffffffff;
+  }
+  else {
+    mask = (1 << (msb+1)) - 1;
+  }  
+  masked = i & mask;
+  bits = masked >> lsb;
+  return bits;
+}
+*/
+
+// https://stackoverflow.com/questions/10090326/how-to-extract-specific-bits-from-a-number-in-c
+static inline unsigned int bit_select(INSTRUCTION i, unsigned int msb, unsigned int lsb)
+{
+  unsigned int mask = ~(~0 << (msb - lsb + 1));
+  unsigned int value = (i >> lsb) & mask;
+  return value;
+}
+
 static inline unsigned int opcode_of(INSTRUCTION i)
 {
-  unsigned int opcode = (i >> 26);
-  return opcode;
+  return bit_select(i, OPCODE_MSB, OPCODE_LSB);
+}
+
+static inline unsigned int rs_of(INSTRUCTION i)
+{
+  return bit_select(i, REG_RS_MSB, REG_RS_LSB);
+}
+
+static inline unsigned int rt_of(INSTRUCTION i)
+{
+  return bit_select(i, REG_RT_MSB, REG_RT_LSB);
+}
+
+static inline unsigned int rd_of(INSTRUCTION i)
+{
+  return bit_select(i, REG_RD_MSB, REG_RD_LSB);
+}
+
+static inline unsigned int imm_of(INSTRUCTION i)
+{
+  return bit_select(i, IMM_MSB, IMM_LSB);
 }
 
 void execute_instruction(INSTRUCTION i)
 {
   unsigned int opcode = opcode_of(i);
+  unsigned int rs = rs_of(i);
+  unsigned int rt = rt_of(i);
+  unsigned int rd = rd_of(i);
+  unsigned int imm = imm_of(i);
+
   switch (opcode) {
+    // 6'b00xxxx
+    case OP_CODE_ADD:
+    case OP_CODE_SUB:
+    case OP_CODE_NOT:
+    case OP_CODE_AND:
+    case OP_CODE_OR:
+    case OP_CODE_NAND:
+    case OP_CODE_NOR:
+    case OP_CODE_MOV:
+    case OP_CODE_SAR:
+    case OP_CODE_SHR:
+    case OP_CODE_SHL:
+    case OP_CODE_XOR:
+    case OP_CODE_TEST:
+    case OP_CODE_CMP:
+      printf("%x %x %x %x\n", opcode, rs, rt, rd);
+      break;
+
+    // 6'b01xxxx
+    case OP_CODE_ADDI:
+    case OP_CODE_SUBI:
+    case OP_CODE_NOTI:
+    case OP_CODE_ANDI:
+    case OP_CODE_ORI:
+    case OP_CODE_NANDI:
+    case OP_CODE_NORI:
     case OP_CODE_MOVI:
-      printf("got an movi\n");
+    case OP_CODE_SARI:
+    case OP_CODE_SHRI:
+    case OP_CODE_SHLI:
+    case OP_CODE_XORI:
+    case OP_CODE_TESTI:
+    case OP_CODE_CMPI:
+      printf("%x %x %x %x\n", opcode, rs, rt, imm);
+      break;
+
+    // 6'b10xxxx
+    case OP_CODE_LW:
+    case OP_CODE_SW:
+    case OP_CODE_LA:
+    case OP_CODE_SA:
+      printf("%x %x %x %x\n", opcode, rs, rt, imm);
+      break;
+
+    // 6'b11xxxx
+    case OP_CODE_JMP:
+    case OP_CODE_JO:
+    case OP_CODE_JE:
+    case OP_CODE_JNE:
+    case OP_CODE_JL:
+    case OP_CODE_JLE:
+    case OP_CODE_JG:
+    case OP_CODE_JGE:
+    case OP_CODE_JZ:
+    case OP_CODE_JNZ:
+    case OP_CODE_JR:
+      printf("%x %x\n", opcode, imm);
+      break;
+
     default:
-      printf("got something else: %d\n", opcode);
+      printf("invalid instruction!");
   }
 }
 
