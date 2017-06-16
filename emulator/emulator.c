e4cc8c6003f31a132463c73251a13868aff2b398
Brian Crafton <crafton.b@husky.neu.edu>
2017-06-15 21:54:14 -0400

    emulator

--- emulator/emulator.c
+++ emulator/emulator.c
@@ -42,6 +42,13 @@ typedef struct instruction{
   unsigned int immediate;
 } instruction_t;
 
+typedef struct program_state{
+  unsigned int pc;
+  unsigned int zero;
+  unsigned int less;
+  unsigned int greater;
+} program_state_t;
+
 // is this really what we want to do here?
 // no this is dumb
 // no need to go through each case
@@ -128,76 +135,193 @@ static inline unsigned int imm_of(INSTRUCTION i)
   return bit_select(i, IMM_MSB, IMM_LSB);
 }
 
-void execute_instruction(INSTRUCTION i)
+void execute_instruction(INSTRUCTION i, program_state_t* p)
 {
   unsigned int opcode = opcode_of(i);
   unsigned int rs = rs_of(i);
   unsigned int rt = rt_of(i);
   unsigned int rd = rd_of(i);
   unsigned int imm = imm_of(i);
+  unsigned int address;
 
   switch (opcode) {
     // 6'b00xxxx
     case OP_CODE_ADD:
+      regfile[rd] = regfile[rs] + regfile[rt];
+      p->pc++;
+      break;
     case OP_CODE_SUB:
+      regfile[rd] = regfile[rs] - regfile[rt];
+      p->pc++;
+      break;
     case OP_CODE_NOT:
+      printf("not implemented yet: not\n");
+      break;
     case OP_CODE_AND:
+      regfile[rd] = regfile[rs] & regfile[rt];
+      p->pc++;
+      break;
     case OP_CODE_OR:
+      regfile[rd] = regfile[rs] | regfile[rt];
+      p->pc++;
+      break;
     case OP_CODE_NAND:
+      regfile[rd] = ~(regfile[rs] & regfile[rt]);
+      p->pc++;
+      break;
     case OP_CODE_NOR:
+      regfile[rd] = ~(regfile[rs] | regfile[rt]);
+      p->pc++;
+      break;
     case OP_CODE_MOV:
+      regfile[rd] = regfile[rt];
+      p->pc++;
+      break;
     case OP_CODE_SAR:
+      // there is no sar in c ...
+      regfile[rd] = regfile[rs] >> regfile[rt];
+      p->pc++;
+      break;
     case OP_CODE_SHR:
+      regfile[rd] = regfile[rs] >> regfile[rt];
+      p->pc++;
+      break;
     case OP_CODE_SHL:
+      regfile[rd] = regfile[rs] << regfile[rt];
+      p->pc++;
+      break;
     case OP_CODE_XOR:
+      regfile[rd] = regfile[rs] ^ regfile[rt];
+      p->pc++;
+      break;
     case OP_CODE_TEST:
+      p->pc++;
+      p->zero = (regfile[rs] & regfile[rt]) == 0;
+      p->less = (regfile[rs] < regfile[rt]);
+      p->greater = (regfile[rs] > regfile[rt]);
+      break;
     case OP_CODE_CMP:
-      printf("%x %x %x %x\n", opcode, rs, rt, rd);
+      p->pc++;
+      p->zero = (regfile[rs] - regfile[rt]) == 0;
+      p->less = (regfile[rs] < regfile[rt]);
+      p->greater = (regfile[rs] > regfile[rt]);
       break;
 
     // 6'b01xxxx
     case OP_CODE_ADDI:
+      regfile[rd] = regfile[rs] + imm;
+      p->pc++;
+      break;
     case OP_CODE_SUBI:
+      regfile[rd] = regfile[rs] - imm;
+      p->pc++;
+      break;
     case OP_CODE_NOTI:
+      printf("not implemented yet: noti\n");
+      break;
     case OP_CODE_ANDI:
+      regfile[rd] = regfile[rs] & imm;
+      p->pc++;
+      break;
     case OP_CODE_ORI:
+      regfile[rd] = regfile[rs] | imm;
+      p->pc++;
+      break;
     case OP_CODE_NANDI:
+      regfile[rd] = ~(regfile[rs] & imm);
+      p->pc++;
+      break;
     case OP_CODE_NORI:
+      regfile[rd] = ~(regfile[rs] | imm);
+      p->pc++;
+      break;
     case OP_CODE_MOVI:
+      regfile[rd] = imm;
+      p->pc++;
+      break;
     case OP_CODE_SARI:
+      // there is no sar in c ...
+      regfile[rd] = regfile[rs] >> imm;
+      p->pc++;
+      break;
     case OP_CODE_SHRI:
+      regfile[rd] = regfile[rs] >> imm;
+      p->pc++;
+      break;
     case OP_CODE_SHLI:
+      regfile[rd] = regfile[rs] << imm;
+      p->pc++;
+      break;
     case OP_CODE_XORI:
+      regfile[rd] = regfile[rs] ^ imm;
+      p->pc++;
+      break;
     case OP_CODE_TESTI:
+      p->pc++;
+      p->zero = (regfile[rs] & imm) == 0;
+      p->less = (regfile[rs] < imm);
+      p->greater = (regfile[rs] > imm);
+      break;
     case OP_CODE_CMPI:
-      printf("%x %x %x %x\n", opcode, rs, rt, imm);
+      p->pc++;
+      p->zero = (regfile[rs] - imm) == 0;
+      p->less = (regfile[rs] < imm);
+      p->greater = (regfile[rs] > imm);
       break;
 
     // 6'b10xxxx
     case OP_CODE_LW:
+      address = regfile[rs] + imm;
+      regfile[rt] = dmemory[address];
+      p->pc++;
+      break;
     case OP_CODE_SW:
+      address = regfile[rs] + imm;
+      dmemory[address] = regfile[rt];
+      p->pc++;
+      break;
     case OP_CODE_LA:
+      break;
     case OP_CODE_SA:
-      printf("%x %x %x %x\n", opcode, rs, rt, imm);
       break;
 
     // 6'b11xxxx
     case OP_CODE_JMP:
+      p->pc = imm;
+      break;
     case OP_CODE_JO:
+      printf("not implemented yet: jo\n");
+      break;
     case OP_CODE_JE:
+      if (p->zero) p->pc = imm;
+      break;
     case OP_CODE_JNE:
+      if (!p->zero) p->pc = imm;
+      break;
     case OP_CODE_JL:
+      if (p->less) p->pc = imm;
+      break;
     case OP_CODE_JLE:
+      if (p->less || p->zero) p->pc = imm;
+      break;
     case OP_CODE_JG:
+      if (p->greater) p->pc = imm;
+      break;
     case OP_CODE_JGE:
+      if (p->greater || p->zero) p->pc = imm;
+      break;
     case OP_CODE_JZ:
+      if (p->zero) p->pc = imm;
+      break;
     case OP_CODE_JNZ:
+      if (!p->zero) p->pc = imm;
+      break;
     case OP_CODE_JR:
-      printf("%x %x\n", opcode, imm);
+      p->pc = regfile[rs];
       break;
 
     default:
-      printf("invalid instruction!");
+      printf("invalid instruction!\n");
   }
 }
 
@@ -205,12 +329,24 @@ int main()
 {
   load_program();
 
+  program_state_t p;
+  p.pc = 0;
+
   int i;
-  for(i=0; i<IMEMORY_SIZE; i++)
+  for(i=0; i<100; i++)
   {
-    //printf("%x\n", opcode_of(imemory[i]));
-    if (imemory[i] != 0)
-      execute_instruction(imemory[i]);
+    //printf("%d\n", imemory[i]);
+    printf("instruction: %x pc: %d\n", imemory[p.pc], p.pc);
+    if (p.pc > IMEMORY_SIZE)
+    {
+      printf("invalid instruction address %d\n", p.pc);
+      break;
+    }
+    execute_instruction(imemory[p.pc], &p);
+  }
+  for(i=0; i<REGFILE_SIZE; i++)
+  {
+    printf("%x\n", regfile[i]);
   }
 }
 
