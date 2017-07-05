63c4ca85c9699f8f86b39d9fc06f16b06cd37cb0
Brian Crafton <crafton.b@husky.neu.edu>
2017-07-05 15:25:55 -0700

    this works but hacky af

--- processor/program_counter.v
+++ processor/program_counter.v
@@ -115,6 +115,10 @@ module program_counter(
     if(flush) begin
       pc <= branch_address;
       branch_taken <= 0;
+    end else if(branch & take_branch) begin
+        pc <= branch_predict;
+        branch_taken <= 1;
+        branch_taken_address <= branch_predict;
     end else if(!stall) begin
       if(reset) begin
         pc <= 0;
@@ -123,9 +127,7 @@ module program_counter(
         pc <= address;
         branch_taken <= 0;
       end else if (branch & take_branch) begin
-        pc <= branch_predict;
-        branch_taken <= 1;
-        branch_taken_address <= branch_predict;
+
       end else begin
         pc <= pc + 2;
         branch_taken <= 0;
